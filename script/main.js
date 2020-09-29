b8cf1c53ead18ab38cebf79bd674a9e9494ea84f
Eldar Akavov <akavoveldar3005@gmail.com>
2020-09-30 00:54:44 +0300

    Create IKEA shop

---
+++ script/main.js
@@ -0,0 +1,45 @@
+'use srict';
+//day 1
+const btnBurger = document.querySelector('.btn-burger'),
+  btnClose = document.querySelector('.btn-close'),
+  subcatalog = document.querySelector('.subcatalog'),
+  subcatalogHeader = document.querySelector('.subcatalog-header')
+  btnReturn = document.querySelector('.btn-return');
+  catalog = document.querySelector('.catalog');
+
+const overlay = document.createElement('div');
+overlay.classList.add('overlay');
+document.body.insertAdjacentElement('beforeend', overlay);
+const openMenu = () => {
+  catalog.classList.add('open');
+  overlay.classList.add('active');
+},
+  closeMenu = () => {
+    closeSubMenu();
+    catalog.classList.remove('open');
+    overlay.classList.remove('active');
+  },
+  openSubMenu = (event) => {
+    event.preventDefault();
+    const itemList = event.target.closest('.catalog-list__item');
+    if(itemList) {
+      subcatalogHeader.innerHTML = itemList.innerHTML;
+      subcatalog.classList.add('subopen');
+    }
+  },
+  closeSubMenu = () => {
+    subcatalog.classList.remove('subopen'); 
+  };
+
+btnBurger.addEventListener('click', openMenu);
+btnClose.addEventListener('click', closeMenu);
+overlay.addEventListener('click', closeMenu);
+catalog.addEventListener('click', openSubMenu);
+btnReturn.addEventListener('click', closeSubMenu);
+
+
+document.addEventListener('keydown', (event) => {
+  if(event.code === 'Escape'){
+    closeMenu();
+  }
+});
